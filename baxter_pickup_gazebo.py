471d234a8ba80393be155c3aa80d29ffdf5904f7
alee112 <alee112@terpmail.umd.edu>
2016-03-10 22:44:00 -0500

    master

--- baxter_pickup_gazebo.py
+++ baxter_pickup_gazebo.py
@@ -74,7 +74,7 @@ class BaxterPickup:
         srv = BaxterIKRequest() 
         srv.pose_stamp.append(PoseStamped(header=hdr, pose=pose)) 
         resp = self._ik(srv)
-        print resp
+        print resp.joint_angles
         # dic = {}
         # for x in resp.joint_angles.name:
         #     dic[resp.joint_angles.name[x]] = resp.joint_angles.position[x]
