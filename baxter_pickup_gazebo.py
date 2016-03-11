600b8f001b64f05e446e42ee6d13d87017255099
alee112 <alee112@terpmail.umd.edu>
2016-03-10 21:58:55 -0500

    master

--- baxter_pickup_gazebo.py
+++ baxter_pickup_gazebo.py
@@ -74,7 +74,7 @@ class BaxterPickup:
         srv = BaxterIKRequest() 
         srv.pose_stamp.append(PoseStamped(header=hdr, pose=pose)) 
         resp = self._ik(srv)
-        print resp
+        self._limb.move_to_joint_positions(resp)
 
     # def move_to_pickup_position(self, point):
     #     # Implement this function - Baxter's limb should be at the target block with it's gripper open but around the target block
